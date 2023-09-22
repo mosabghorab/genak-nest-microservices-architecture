9017f50eb7220af01e42025ba32071c7d15ac0cb
mosabghorab <mosabghorab2000@gmail.com>
2023-09-23 02:02:55 +0300

    continue :)

--- libs/common/src/modules/custom-config/custom-config.module.ts
+++ libs/common/src/modules/custom-config/custom-config.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule } from '@nestjs/config';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-      envFilePath: '.env.' + (process.env.NODE_ENV || 'development'),
+      envFilePath: '.env',
     }),
   ],
 })
